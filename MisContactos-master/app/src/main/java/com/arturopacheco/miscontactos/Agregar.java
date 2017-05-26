2e2baf9e1b2adea04848fd60d231f86d4508e5eb
darkeyez <ericklopez151294@hotmail.com>
2017-05-26 08:49:30 -0500

    contacto
    Lopez Salas Erick y Catalán Canales Elsa Fabiola

---
+++ MisContactos-master/app/src/main/java/com/arturopacheco/miscontactos/Agregar.java
@@ -0,0 +1,9 @@
+package com.arturopacheco.miscontactos;
+
+/**
+ * Created by Erick y elsa on 26/05/2017.
+ */
+
+public class Agregar (View view){
+    Contacto contacto = new Contacto (nombre_input.getText().toString)
+}
---
+++ MisContactos-master/app/src/main/java/com/arturopacheco/miscontactos/Eliminar.java
@@ -0,0 +1,8 @@
+package com.arturopacheco.miscontactos;
+
+/**
+ * Created by Erick on 26/05/2017.
+ */
+
+public class Eliminar {
+}
